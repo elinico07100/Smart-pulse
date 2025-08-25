b3ff0d8bc25b5b619ef278560bcf2b005133431a
elinico07100 <elinico07100@gmail.com>
2025-08-25 10:46:16 -0300

    terminando

--- smart/RecibePulso.py
+++ smart/RecibePulso.py
@@ -54,4 +54,4 @@ if __name__ == '__main__':
     
     # Iniciar servidor Flask
     print("Iniciando servidor Flask en http://localhost:5000")
-    app.run(debug=True, host='0.0.0.0', port=5000)
+    app.run(debug=True, host='0.0.0.0', port=5000, use_reloader=False)
